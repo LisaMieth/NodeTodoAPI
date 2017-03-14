a8bbbc4faa097ed6a575f7bfb90cb6a9ef3fd2a0
LisaMieth <lisahafizovic@gmail.com>
2017-03-14 13:02:45 +1100

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku:nodetodoapi@ds145389.mlab.com:45389/node-todo-api');
+mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
+
+//mongoose.connect('mongodb://heroku:nodetodoapi@ds145389.mlab.com:45389/node-todo-api');
+
 
 module.exports = {mongoose};
