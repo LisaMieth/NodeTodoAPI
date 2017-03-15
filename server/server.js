f465eb7bca48846b19feb5f20ec7835ca767ab48
LisaMieth <lisahafizovic@gmail.com>
2017-03-15 15:49:21 +1100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+// var env = process.env.NODE_ENV || 'development';
+//
+// console.log('env', env);
+//
+// if(env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+// }
+// else if (env ==='test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+// }
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -8,7 +21,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ||3000;
 
 app.use(bodyParser.json());
 
@@ -20,7 +33,7 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (err) => {
     res.status(400).send(err);
-  })
+  });
 });
 
 app.get('/todos', (req,res) => {
@@ -87,6 +100,19 @@ app.patch('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
+  });
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
   });
 });
 
